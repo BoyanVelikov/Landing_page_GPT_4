0ac104de08f829777a66d4f4b861d25956b689d1
Boyan Velikov <boyanvelikov@gmail.com>
2023-04-02 08:57:14 +0300

    1 year change

--- index.js
+++ index.js
@@ -16,4 +16,24 @@ function generatePassword(length, complexity) {
     }
     return password;
   }
+
+
+  function generatePassword(length, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_-+=';
+    
+    let chars = lowercaseChars;
+    if (includeUppercase) chars += uppercaseChars;
+    if (includeNumbers) chars += numberChars;
+    if (includeSymbols) chars += symbolChars;
+    
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+  }
+  
   
